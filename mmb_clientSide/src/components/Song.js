68fbc5eeacbdfff6c8f6e6470fb53de8a46e997b
shivamkgpian <shivamkval@gmail.com>
2023-03-09 01:32:02 +0530

    Footer navbar created

--- mmb_clientSide/src/components/Song.js
+++ mmb_clientSide/src/components/Song.js
@@ -4,6 +4,7 @@ import "./Song.css";
 import { Link } from "react-router-dom";
 import ReactPlayer from "react-player";
 import NavbarComp from './Navbar2'
+import Navbar3 from "./Navbar3";
 import Footer from './Footer'
 import Chatapp from './Chatapp'
 import Chat from "./Chat";
@@ -13,12 +14,12 @@ let done = false;
 
 const Song = ({ song }) => {
   const [currSongIndex, setCurrSongIndex] = useState(0);
-  
-  useEffect(() => {}, [currSongIndex]);
+
+  useEffect(() => { }, [currSongIndex]);
   useLayoutEffect(() => {
-    window.scrollTo(0,0)
-}
-  
+    window.scrollTo(0, 0)
+  }
+
   );
   console.log(song);
   const shuffledSong = song;
@@ -31,56 +32,59 @@ const Song = ({ song }) => {
 
   useEffect(() => {
     document.title = "Now Playing | MakeMyBrain"
-}, [])
+  }, [])
 
-// Play and pause button
-const [playing, setPlaying] = useState(1)
-function play_pause() {
-  if (playing === 1) {
-    setPlaying(0);
+  // Play and pause button
+  const [playing, setPlaying] = useState(1)
+  function play_pause() {
+    if (playing === 1) {
+      setPlaying(0);
+    }
+    else {
+      setPlaying(1)
+    }
   }
-  else {
-    setPlaying(1)
-  }
-}
 
 
   return (
     <div id="newNavFlex">
 
       <header>
-      <NavbarComp></NavbarComp>
+        <NavbarComp></NavbarComp>
       </header>
-      <main  id="song_chat">
-      <div>
+      <main id="song_chat">
+        <div>
 
-        <h1 id="song_heading">Now Playing</h1>
-        <ReactPlayer
-          className="react_player"
+          <h1 id="song_heading">Now Playing</h1>
+          <ReactPlayer
+            className="react_player"
           /*onEnded={}*/ playing={playing}
-          width="360px"
-          height="0px"
-          url={shuffledSong[currSongIndex].lind}
-          onEnded={playNextSong}
-        />
-        <div id="player">
-          <div id="song_name">{shuffledSong[currSongIndex].title}</div>
-          <div id="singer_name">{shuffledSong[currSongIndex].singer}</div>
-        </div>
-        <button onClick={play_pause} id="click_button">Play | Pause</button>
-        <button onClick={playNextSong} type="submit" id="click_button">
-          Next Song
-        </button>
-        
-        
-        
+            width="360px"
+            height="0px"
+            url={shuffledSong[currSongIndex].lind}
+            onEnded={playNextSong}
+          />
+          <div id="player">
+            <div id="song_name">{shuffledSong[currSongIndex].title}</div>
+            <div id="singer_name">{shuffledSong[currSongIndex].singer}</div>
+          </div>
+          <button onClick={play_pause} id="click_button">Play | Pause</button>
+          <button onClick={playNextSong} type="submit" id="click_button">
+            Next Song
+          </button>
 
-      </div>
-      {/*<div id="chat-window">
+
+
+
+        </div>
+        {/*<div id="chat-window">
         <Chatapp></Chatapp>
 </div>*/}
       </main>
-      
+      <footer className="nav3">
+        <Navbar3></Navbar3>
+      </footer>
+
     </div>
   );
 };
