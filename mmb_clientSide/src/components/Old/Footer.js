54df419cd360cf48847bd764f8474eeb06753508
nkraj7266 <nkraj7266@gmail.com>
2023-05-30 11:36:44 +0530

    new hompage

---
+++ mmb_clientSide/src/components/Old/Footer.js
@@ -0,0 +1,76 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { FaCopyright, FaFacebookSquare, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
+import './Footer.css';
+import logo from '../media/apple-touch-icon.png'
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer p-3">
+                <Container>
+                    <Row className="justify-content-md-center text-white">
+                        <Col sm={3}>
+                            <h5>About Us</h5>
+                            <p id="footer_text">MakeMyBrain aims to empower you to express your emotions 
+                                and thoughts to the fullest, without the fear of being 
+                                judged.
+                            </p>
+                        </Col>
+                        <Col sm={3}>
+                            <h5>Navigation</h5>
+                            <ul >
+                                <li><Link to={"/category"} className="text-white text-decoration-none">Mind Tales</Link></li>
+                                <li><Link to={"/openup"} className="text-white text-decoration-none">Feel Better</Link></li>
+                                <li><Link to={"/about"} className="text-white text-decoration-none">About Us</Link></li>
+                                <li><Link to={"/therapist"} className="text-white text-decoration-none">Find Therapist</Link></li>
+                            </ul>
+                        </Col>
+                        <Col sm={3}>
+                            <Row >
+                                <div className="d-flex justify-content-start" >
+                                    <img src={logo}
+                                        width="60"
+                                        height="60"
+                                        alt="" />
+                                    <div className="ms-3">
+                                        <h3>MakeMyBrain</h3>
+                                        <hr />
+                                        <h6 style={{fontWeight:'400'}}>BECAUSE YOU MATTER</h6>
+                                    </div>
+                                </div>
+                                <div>
+
+                                </div>
+                            </Row>
+                            <Row >
+                                <h7>Connect with us</h7>
+                                <Col >
+                                    <a href=""><FaFacebookSquare className="text-white ms-1" size='40' /></a>
+                                </Col>
+                                <Col >
+                                    <a href="https://www.instagram.com/makemybrain_india/"><FaInstagram className="text-white " size='40' /></a>
+                                </Col>
+                                <Col >
+                                    <a href="https://www.linkedin.com/company/makemybrain/"><FaLinkedin className="text-white ms-1" size='40' /></a>
+                                </Col>
+                                <Col >
+                                    <a href="https://www.youtube.com/@makemybrain"><FaYoutube className="text-white ms-1" size='40' /></a>
+                                </Col>
+                            </Row>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+            <div className="footerbottom p-2">
+                <Container>
+                    <p className="text-center text-white mt-3">Copyright <FaCopyright /> 2023, MakeMyBrain. All Rights Reserved.</p>
+                </Container>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ mmb_clientSide/src/components/Old/Navbar.js
@@ -0,0 +1,75 @@
+import { Nav, Navbar, Container } from 'react-bootstrap';
+import './navbar.css'
+import logo from '../media/logo.png'
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+import Home from './Home';
+import AboutUs from './Aboutus';
+import Blog from './Blogs';
+import Faq from './Faq';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+
+const NavbarComp = () => {
+    const navigate = useNavigate();
+    
+    const handleAuth = () => {
+        
+        if (localStorage.getItem("jwt_token")) {
+            const jwtToken = localStorage.getItem("jwt_token")
+            console.log(jwtToken);
+            axios
+                .post(`https://mmb-auth.onrender.com/users/logout`,null,  {
+                    headers: {
+                        Authorization: `Bearer ${jwtToken}`
+                    }
+                })
+                .then((res) => {
+                    localStorage.removeItem("email");
+                    localStorage.removeItem("subs");
+                    localStorage.removeItem("jwt_token");
+                    navigate("/");
+                })
+                .catch((error) => {
+                    console.log("error.message", error.message);
+                });
+        }
+        else {
+            navigate('/login');
+        }
+    }
+    return (
+        <div id='header' >
+            <Navbar expand="lg" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to={"/"} className="fs-4">
+                        <img src={logo}
+                            width="40"
+                            height="40"
+                            className="d-inline-block align-top" />{' '}
+                        MakeMyBrain
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav" >
+                        <Nav expand="lg" className='ma-auto gap-3  font-Segoe UI'>
+
+                            <Nav.Link as={Link} to={"/openup"} id='talk_button'>Get Solution</Nav.Link>
+                            <Nav.Link as={Link} to={"/category"}>Blogs</Nav.Link>
+                            {/* <a href='https://makemybrain.com/blog' id='blog_link'>Blogs</a> */}
+                            <Nav.Link as={Link} to={"/therapist"}>Find Therapist</Nav.Link>
+                            <Nav.Link as={Link} to={"/about"}>About Us</Nav.Link>
+                            <Nav.Link onClick={handleAuth}>{localStorage.getItem("jwt_token") ? 'Logout' : 'Login'}</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+}
+
+export default NavbarComp;
