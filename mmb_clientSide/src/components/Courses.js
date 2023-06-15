ff1f19c6ea9d5c78ead4ea43e7a75394c9137f3e
nkraj7266 <nkraj7266@gmail.com>
2023-06-15 19:27:29 +0530

    Made courses page responsive

--- mmb_clientSide/src/components/Courses.js
+++ mmb_clientSide/src/components/Courses.js
@@ -1,8 +1,7 @@
 import Styles from "./Courses.module.css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import React, { useState, useEffect } from "react";
 import { Modules } from "./Modules";
+import ReactPlayer from "react-player/youtube";
 import Footer from "./Footer";
 
 const Courses = () => {
@@ -10,27 +9,58 @@ const Courses = () => {
 	const [selectedChapter, setSelectedChapter] = useState({
 		heading: "Hi! You're Welcome to the course",
 		videoLink: "https://www.youtube.com/embed/dWm8PXlzDpI",
-		content: "We begin the course with an emotional hindi poem on mental health. Hope you learn a lot from this course that has been carefully created through research. Happy Learning!",
+		content:
+			"We begin the course with an emotional hindi poem on mental health. Hope you learn a lot from this course that has been carefully created through research. Happy Learning!",
 	});
+	const [showSideList, setShowSideList] = useState(false);
 
 	useEffect(() => {
 		window.scrollTo(0, 0);
 	}, []);
+
 	useEffect(() => {
-        document.title = "Mind Your Mind - Mental Health Wellness Course | MakeMyBrain"
-      }, [])
+		document.title =
+			"Mind Your Mind - Mental Health Wellness Course | MakeMyBrain";
+	}, []);
+
 	const handleModuleClick = (index) => {
 		setOpenModule(index === openModule ? null : index);
 		setSelectedChapter({
 			heading: "Hi! You're Welcome to the course",
 			videoLink: "https://www.youtube.com/embed/dWm8PXlzDpI",
-			content: "We begin the course with an emotional hindi poem on mental health. Hope you learn a lot from this course that has been carefully created through research. Happy Learning!",
+			content:
+				"We begin the course with an emotional hindi poem on mental health. Hope you learn a lot from this course that has been carefully created through research. Happy Learning!",
 		});
 	};
 
 	const handleChapterClick = (chapter) => {
 		setSelectedChapter(chapter);
+		if (window.innerWidth <= 768) {
+			setShowSideList(false);
+		} else {
+			setShowSideList(true);
+		}
 	};
+
+	const handleArrowButtonClick = () => {
+		setShowSideList(!showSideList);
+	};
+
+	useEffect(() => {
+		const handleWindowResize = () => {
+			if (window.innerWidth <= 768) {
+				setShowSideList(false);
+			} else {
+				setShowSideList(true);
+			}
+		};
+
+		window.addEventListener("resize", handleWindowResize);
+
+		return () => {
+			window.removeEventListener("resize", handleWindowResize);
+		};
+	}, []);
 
 	return (
 		<div className={Styles["container"]}>
@@ -44,63 +74,81 @@ const Courses = () => {
 			</div>
 			<div>
 				<main className={Styles["course-details"]}>
-					<nav className={Styles["course-outline"]}>
-						<div className={Styles["course-outline-wrap"]}>
-							<div className={Styles["intro-text"]}>
-								<h2>Contents</h2>
-							</div>
-							{Modules.map((module, index) => (
-								<div key={index} className={Styles["unit"]}>
-									<div
-										onClick={() => handleModuleClick(index)}
-										className={Styles["unit-title"]}
-									>
-										<span>{module.name}</span>
-									</div>
-									<ul
-										className={`${Styles["lessons-list"]} ${
-											openModule === index
-												? Styles.open
-												: ""
-										}`}
-									>
-										{module.chapters.map(
-											(chapter, chapterIndex) => (
-												<li
-													key={chapterIndex}
-													className={Styles["lesson"]}
-													onClick={() =>
-														handleChapterClick(
-															chapter
-														)
-													}
-												>
-													<span>
-														{chapter.heading}
-													</span>
-												</li>
-											)
-										)}
-									</ul>
+					{showSideList && (
+						<nav className={Styles["course-outline"]}>
+							<div className={Styles["course-outline-wrap"]}>
+								<div className={Styles["intro-text"]}>
+									<h2>Contents</h2>
 								</div>
-							))}
-						</div>
-					</nav>
+								{Modules.map((module, index) => (
+									<div key={index} className={Styles["unit"]}>
+										<div
+											onClick={() =>
+												handleModuleClick(index)
+											}
+											className={Styles["unit-title"]}
+										>
+											<span>{module.name}</span>
+										</div>
+										<ul
+											className={`${
+												Styles["lessons-list"]
+											} ${
+												openModule === index
+													? Styles.open
+													: ""
+											}`}
+										>
+											{module.chapters.map(
+												(chapter, chapterIndex) => (
+													<li
+														key={chapterIndex}
+														className={
+															Styles["lesson"]
+														}
+														onClick={() =>
+															handleChapterClick(
+																chapter
+															)
+														}
+													>
+														<span>
+															{chapter.heading}
+														</span>
+													</li>
+												)
+											)}
+										</ul>
+									</div>
+								))}
+							</div>
+						</nav>
+					)}
+
+					<div
+						className={`${Styles["arrow-button"]} ${
+							!showSideList ? "" : Styles["shift"]
+						}`}
+						onClick={handleArrowButtonClick}
+					>
+						<span>{showSideList ? "<" : ">"}</span>
+					</div>
+
 					<section className={Styles["course-content"]}>
 						<div className={Styles["intro-text"]}>
 							<h2>{selectedChapter.heading}</h2>
 						</div>
 						{selectedChapter && (
 							<div className={Styles["video-container"]}>
-								<iframe
-									src={selectedChapter.videoLink}
-									// width="531" // 295
-									// height="299" // 166
-									title={selectedChapter.title}
-									frameborder="0"
-									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-									allowfullscreen
-								/>
+								<div className={Styles["player-wrapper"]}>
+									<ReactPlayer
+										className={Styles["react-player"]}
+										url={selectedChapter.videoLink}
+										controls
+										width="100%"
+										height="100%"
+									/>
+								</div>
 								<p className={Styles["chapter-descp"]}>
 									{selectedChapter.content}
 								</p>
@@ -108,8 +156,7 @@ const Courses = () => {
 						)}
 					</section>
 				</main>
-				
-			</div><br/><br/><br/>
+			</div>
 			<Footer></Footer>
 		</div>
 	);
